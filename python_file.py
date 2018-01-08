a1b66ee8f91946406fe19b97a6d0e94f1fdb5868
Ngenge Senior <seniorngenge@gmail.com>
2018-01-08 09:47:47 +0100

    Added new line

--- python_file.py
+++ python_file.py
@@ -2,4 +2,5 @@ print("Python is better than other langs")
 print("Python follows a convention called PEP8")
 print("This new line after PEP8 stuff was added on local PC")
 print("This change has been made on dev branch on PC")
+print("I just added this again on dev. I will merge it to master")
 
